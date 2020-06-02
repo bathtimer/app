6a4c8cba49378afdea58d934aca57c44cc6a026a
Brian Do <briancldo@gmail.com>
2020-06-02 18:58:31 -0400

    Updates

---
+++ precache-manifest.559cb0f78ff27ae10035f070cd8522a0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "92eb7e18d0e29429e0d2",
+    "url": "/static/js/app.b58fbe90.chunk.js"
+  },
+  {
+    "revision": "e91487e8c8aa62ce91ce",
+    "url": "/static/js/runtime~app.34c76111.js"
+  },
+  {
+    "revision": "e59185f98c0c11a2cd17",
+    "url": "/static/js/2.cb65bc0e.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "0cbca57a28ee134f8c079bccdbfb2469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d25816b015703fde2793f8f1f0e542b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];
